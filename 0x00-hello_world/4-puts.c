a0352a693795ad2aeb91cd8db098ccd24baaf0b0
MercyjohnN <Mercyjohnc11@gmail.com>
2022-10-14 23:29:03 -0700

    Task eight

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,13 @@
 /**
 * main - Entry
 *
-* Return: Always 0 (Success)
+* Return: Always 0 
 */
 
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
         return (0);
-}
+
+}	          
 
