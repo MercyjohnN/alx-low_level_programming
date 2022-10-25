5f4fa609ccece15faffab5945fb5245a5d5af105
MercyjohnN <Mercyjohnc11@gmail.com>
2022-10-25 06:30:59 -0700

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,9 +1,6 @@
 include < stdio.h >
-
 #include <stdlib.h>
-
 #include <time.h>
-
 /**
  * main - Generates random valid passwords for the
  * program 101-crackme.
@@ -13,11 +10,9 @@ include < stdio.h >
 int main(void)
 {
 	char password[84];
-
 	int index = 0, sum = 0, diff_half1, diff_half2;
 
 	srand(time(0));
-
 	while (sum < 2772)
 	{
 		password[index] = 33 + rand() % 94;
