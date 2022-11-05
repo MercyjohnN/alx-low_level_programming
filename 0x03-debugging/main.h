b9275168cd1c70a4ee556941f78e22d68b7313be
MercyjohnN <Mercyjohnc11@gmail.com>
2022-11-05 09:30:44 -0700

    Task 1

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,18 +1,9 @@
+#ifndef main_h
+#define main_h
 #include <stdio.h>
-/**
- * main - causes an infinite loop
- * Return: 0
- */
-int main(void)
-{
-	int i;
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
-	printf("Infinite loop incoming :(\n");
-	i = 0;
-	/*while (i < 10)*/
-	/*{*/
-	/*putchar(i);*/
-	/*}*/
-	printf("Infinite loop avoided! \\o/\n");
-	return (0);
-}
+#endif
